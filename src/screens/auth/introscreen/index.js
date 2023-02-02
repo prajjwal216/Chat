98f022c1be630c0851def24de663da3f0b44af02
Tarun <tarunpal982612+git@gmail.com>
2023-02-02 16:53:00 +0530

    font and introscreen

--- src/screens/auth/introscreen/index.js
+++ src/screens/auth/introscreen/index.js
@@ -1,14 +1,31 @@
+import {Image, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import {Text, View, StyleSheet, Button} from 'react-native';
+import styles from './style';
+import IMAGE from '../../../../assets/images';
+import CustomButton from '../../../components/common/CustomButton';
 
-export default Introscreen = ({navigation}) => {
+const Introscreen = ({navigation}) => {
+  const text =
+    'Connect easily with your family and\nfriends worldwide with Void';
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>introscreen</Text>
-      <Button
-        title="goto Signup"
-        onPress={() => navigation.navigate('Signup')}
-      />
+    <View style={styles.View}>
+      <View style={styles.topView}>
+        <View style={styles.imageView}>
+          <Image source={IMAGE.welcomeImage} />
+        </View>
+        <View style={styles.TextView}>
+          <Text style={styles.welcomeText}>{text}</Text>
+        </View>
+      </View>
+      <View style={styles.bottomView}>
+        <Text style={styles.Policy}>Terms & Privacy Policy</Text>
+        <CustomButton
+          title={'Start Messaging'}
+          onPress={() => navigation.navigate('Signup')}
+        />
+      </View>
     </View>
   );
 };
+
+export default Introscreen;
