aa2819d536cce707fd6e03bd214aceb4ae58d6fd
Tarun <tarunpal982612+git@gmail.com>
2023-02-02 18:41:20 +0530

    tab bar changes

--- src/navigator/tabnavigator/index.js
+++ src/navigator/tabnavigator/index.js
@@ -4,12 +4,40 @@ import Settings from '../../screens/dashboard/settings';
 import Calls from '../../screens/dashboard/call';
 import Chat from '../../screens/dashboard/chat';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import GLOBALS from '../../../assets/index';
+const {FONTS, COLOR} = GLOBALS;
 
 const Tab = createBottomTabNavigator();
 
 export default function Tabnavigation() {
   return (
-    <Tab.Navigator initialRouteName="Home">
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={({route, navigation}) => ({
+        tabBarShowLabel: false,
+        headerTitleAlign: 'center',
+        headerShown: false,
+        tabBarActiveBackgroundColor: COLOR.PRIMARY,
+        // headerStyle: {
+        //   backgroundColor: '',
+        //   borderBottomLeftRadius: 30,
+        //   borderBottomRightRadius: 30,
+        //   height: 60,
+        // },
+
+        tabBarStyle: {
+          borderTopLeftRadius: 20,
+          borderTopRightRadius: 20,
+          borderBottomRightRadius: 20,
+          borderBottomLeftRadius: 20,
+        },
+        tabBarHideOnKeyboard: true,
+        tabBarActiveTintColor: 'white',
+        headerTintColor: 'white',
+        headerTitleStyle: {
+          fontSize: 25,
+        },
+      })}>
       <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Chat" component={Chat} />
       <Tab.Screen name="Calls" component={Calls} />
--- src/screens/dashboard/home/index.js
+++ src/screens/dashboard/home/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
+import COLOR from '../../../../assets/color';
 
 export default Home = () => {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>chat page</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: COLOR.GREEN,
+      }}>
+      <Text>Home page</Text>
     </View>
   );
 };
