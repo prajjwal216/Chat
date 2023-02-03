15aed7628a1306a021454da6ffd9b0cac325190f
Nikhil Arora <nikhilarora@SDD-83.local>
2023-02-03 10:09:29 +0530

    signup

--- src/screens/auth/signup/index.js
+++ src/screens/auth/signup/index.js
@@ -1,15 +1,29 @@
 import React from 'react';
-import {Text, View, StyleSheet, Button} from 'react-native';
+import CustomTextInput from '../../../components/common/Custominput';
+import {Text, View, Button, TextInput} from 'react-native';
+import styles from './style';
+import CustomButton from '../../../components/common/CustomButton';
 
 export default Signup = ({navigation}) => {
+  const text =
+    'Please confirm your country code and enter\nyour phone number/email';
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Signup</Text>
+    <View style={styles.mainView}>
+      <View style={{justifyContent: 'center', alignItems: 'center', flex: 1}}>
+        <Text style={styles.mainHeading}>Enter Your Email / Phone Number</Text>
+        <Text style={styles.text}>{text}</Text>
 
-      <Button
-        title="Continue"
-        onPress={() => navigation.navigate('Phoneverification')}
-      />
+        <View style={styles.numberView}>
+          <TextInput style={styles.code} />
+          <TextInput style={styles.number} placeholder={'Phone Number'} />
+        </View>
+      </View>
+      <View style={styles.bottomView}>
+        <CustomButton
+          title={'Continue'}
+          onPress={() => navigation.navigate('Phoneverification')}
+        />
+      </View>
     </View>
   );
 };
