8014bb2738099717877a1e1d114aef2108949445
Tarun <tarunpal982612+git@gmail.com>
2023-02-07 13:08:44 +0530

    phone otp verification page

--- src/navigator/authnavigator/index.js
+++ src/navigator/authnavigator/index.js
@@ -3,7 +3,6 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Signup from '../../screens/auth/signup';
 import Introscreen from '../../screens/auth/introscreen';
 import Phoneverification from '../../screens/auth/phoneverification';
-import Emailverification from '../../screens/auth/emailverification/index';
 import Createprofile from '../../screens/auth/createprofile';
 import Tabnavigation from '../tabnavigator';
 import Chatscreen from '../../screens/dashboard/chatscreen';
@@ -19,7 +18,6 @@ export default function Authnavigation() {
       <Stack.Screen name="Introscreen" component={Introscreen} />
       <Stack.Screen name="Signup" component={Signup} />
       <Stack.Screen name="Phoneverification" component={Phoneverification} />
-      <Stack.Screen name="Emailverification" component={Emailverification} />
       <Stack.Screen name="Createprofile" component={Createprofile} />
       <Stack.Screen name="Tabnavigation" component={Tabnavigation} />
       <Stack.Screen name="Chatscreen" component={Chatscreen} />
--- src/screens/auth/phoneverification/index.js
+++ src/screens/auth/phoneverification/index.js
@@ -1,15 +1,41 @@
 import React from 'react';
-import {Text, View, StyleSheet, Button} from 'react-native';
+import {Text, View} from 'react-native';
+import styles from './style';
+import CustomButton from '../../../components/common/CustomButton';
+import OTPInputView from '@twotalltotems/react-native-otp-input';
 
 export default Phoneverification = ({navigation}) => {
-  return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Phoneverification</Text>
+  const text = 'We have sent you an SMS with the code';
 
-      <Button
-        title="Continue"
-        onPress={() => navigation.navigate('Emailverification')}
-      />
+  return (
+    <View style={styles.mainView}>
+      <View
+        style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          flex: 1,
+        }}>
+        <Text style={styles.mainHeading}>Enter Your Verification</Text>
+        <Text style={styles.text}>{text}</Text>
+
+        <OTPInputView
+          style={{width: '50%', height: 200, marginRight: 20}}
+          pinCount={4}
+          autoFocusOnLoad
+          codeInputFieldStyle={styles.underlineStyleBase}
+          codeInputHighlightStyle={styles.underlineStyleHighLighted}
+          onCodeFilled={code => {
+            <Text style={{}}>`Code is ${code}, you are good to go!`</Text>;
+          }}
+        />
+      </View>
+
+      <View style={styles.bottomView}>
+        <CustomButton
+          title={'Continue'}
+          onPress={() => navigation.navigate('Createprofile')}
+        />
+      </View>
     </View>
   );
 };
